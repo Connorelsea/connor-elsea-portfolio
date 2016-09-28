d30bd94c9ded8e5c2bb2799a8f704ef917ea4cde
Connorelsea <connorelsea@gmail.com>
2016-09-28 14:33:57 -0500

    Some quick responsive fixes. Still no contact info on mobile

--- src/components/OpenSource.js
+++ src/components/OpenSource.js
@@ -4,44 +4,13 @@ import WorkItem from "./WorkItem"
 class OpenSource extends Component {
   render() {
     return (
-      <div className="Open-Source">
+      <div className="Page">
 
-        <p>
-          This online resume is made with React and React Router v4. Check it, and my other projects, out on <a href="https://github.com/Connorelsea/connor-elsea-portfolio">Github!</a>
-        </p>
+        <div className="Page__Bubble">
+          <h2>Coming Soon</h2>
+          <p>You can learn more about my open source projects on my <a href="https://github.com/Connorelsea">Github profile</a>. In the near future I will feature my projects and pictures on this page as well.</p>
+        </div>
 
-        <WorkItem
-          company="JavaScript Air"
-          dates="Summer 2016"
-          title=""
-        >
-          <div className="Work-Item__Content__Github">View on <a href="https://github.com/javascriptair/site">Github!</a></div>
-          <p>Designed and implemented new branding for JavaScript Air Podcast using React</p>
-        </WorkItem>
-
-        <WorkItem
-          company="Board"
-          dates="Summer 2016"
-          title=""
-        >
-          <div className="Work-Item__Content__Github">View on <a href="https://github.com/Connorelsea/lsmsa-board-backend">Github!</a></div>
-          <p>- Created student tracking and sign out system for local boarding school system</p>
-          <p>- Offline-capable web application using ES6, React, Redux, MySQL, Node 6.x+, Babel, Webpack, etc.</p>
-          <p>- Custom REST API utilities built alongside Knex and Bookshelf ORM to allow for rapid iteration on Node REST APIs</p>
-        </WorkItem>
-
-        <WorkItem
-          company="LSMSA SGO"
-          dates="Summer 2015"
-          title=""
-        >
-          <div className="Work-Item__Content__Github">View on <a href="https://github.com/Connorelsea/LSMSA-SGO-Website">Github!</a></div>
-          <p>Created a interactive student government voting platform for my school, with cafeteria ratings, legislation discussion. All at the request of the administration</p>
-        </WorkItem>
-
-        <p>
-          These are only my most recent projects. There are many more on my <a href="https://github.com/Connorelsea/connor-elsea-portfolio">Github!</a>
-        </p>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -8,7 +8,17 @@ import { BrowserRouter, Match } from "react-router"
 ReactDOM.render(
   <BrowserRouter>
     <div>
+
+      <div className="App__Info">
+        <p>Contact me: </p>
+        <a href="#">Github</a>
+        <a href="#">LinkedIn</a>
+        <a href="#">Twitter</a>
+        <a href="#">Email</a>
+      </div>
+
       <Match pattern="/" component={App} />
+
     </div>
   </BrowserRouter>,
   document.getElementById('root')

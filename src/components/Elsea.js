3500bbbe76563946141d9ec19c816598c24eb770
Connorelsea <connorelsea@gmail.com>
2016-09-28 10:53:35 -0500

    Refactor for new WorkItem

--- src/components/Elsea.js
+++ src/components/Elsea.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react'
 import WorkItem from "./WorkItem"
 
+const data = [
+  {
+    company: "Elsea Labs",
+    dates: "February 2011 - Present",
+    title: "Founder, CEO, Softare Developer",
+    tech: ["React", "Redux", "JavaScript/ES6", "CSS/BEM", "Node.JS", "MySQL"],
+    bullets: [
+      "Founded in January 2011 to work with clients to create business software solutions",
+      "Provided development and graphic design services to local and regional clients",
+      "Desktop Development: Java 8, Scala, JVM Technologies, JavaFX",
+      "Web Development: Node.JS, JavaScript (ES6), React, Redux, Flux, HTML, CSS",
+    ]
+  }
+]
+
 class Elsea extends Component {
   render() {
     return (
       <div className="Elsea">
-        <WorkItem
-          company="Elsea Labs"
-          dates="February 2011 - Present"
-          title="Founder, CEO, Software Engineer"
-        >
-          <p>- Founded in January 2011 to work with clients to create business software solutions</p>
-          <p>- Provided development and graphic design services to local and regional clients</p>
-          <p>- Desktop Development: Java 8, Scala, JVM Technologies, JavaFX</p>
-          <p>- Web Development: Node.JS, JavaScript (ES6), React, Redux, Flux, HTML, CSS</p>
-        </WorkItem>
+        {data.map((item, i) => <WorkItem key={i} {...item} />)}
       </div>
     )
   }
--- src/components/JuniorDeveloper.js
+++ src/components/JuniorDeveloper.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react'
 import WorkItem from "./WorkItem"
 
+const data = [
+  {
+    company: "New Aperio",
+    dates: "May 2016 - Present",
+    title: "Junior Developer",
+    tech: ["React", "Redux", "JavaScript/ES6", "CSS/BEM", "Ruby on Rails", "Node.JS", "PostgreSQL"],
+    bullets: [
+      "Worked on an tight-knit, agile team developing medical software for LOPA, PBRC, and other large companies",
+      "Developed complex and performant React components using Redux, JSX, ES6",
+      "Used modern HTML 5, SCSS, and BEM to develop a easily-refactorable and easy-to-maintain frontend",
+      "Worked with PostgreSQL and Rails ActiveRecord to design and maintain databases",
+      "Worked with Ruby 2.3+ and Rails 5 to model data and create an JSON REST API-based backend"
+    ]
+  }
+]
+
 class JuniorDeveloper extends Component {
   render() {
     return (
-      <div className="JuniorDeveloper">
-        <WorkItem
-          company="New Aperio"
-          dates="May 2016 - Present"
-          title="Junior Developer"
-        >
-          <p>- Worked on an tight-knit, agile team developing medical software for LOPA, PBRC, and other large companies</p>
-          <p>- Developed complex and performant React components using Redux, JSX, ES6</p>
-          <p>- Used modern HTML 5, SCSS, and BEM to develop a easily-refactorable and easy-to-maintain frontend</p>
-          <p>- Worked with PostgreSQL and Rails ActiveRecord to design and maintain databases</p>
-          <p>- Worked with Ruby 2.3+ and Rails 5 to model data and create an JSON REST API-based backend</p>
-        </WorkItem>
+      <div className="Page">
+
+        <div className="Page__Bubble">
+          <h2>Note</h2>
+          <p>This is my current and most recent position, but I'm still doing work on the side for freelance and open source. </p>
+        </div>
+
+        {data.map((item, i) => <WorkItem key={i} {...item} />)}
       </div>
     )
   }
